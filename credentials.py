d9f19ae762f500365c7d7eac5131c857dfba74b5
Suad shire <jido@abdulazizs-MacBook-Air.local>
2019-12-17 14:59:26 +0300

    added a function for save data

--- credentials.py
+++ credentials.py
@@ -14,3 +14,7 @@ def __init__(self, app_title, acc_name, acc_password):
         self.app_title = app_title
         self.acc_name = acc_name
         self.acc_password = acc_password
+
+        def save_data(self):
+            '''Method that saves our accounts'''
+
