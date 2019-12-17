0d4f0bcf3d1bfd9f0f6b590cd52218bf3d0d9ceb
Suad shire <jido@abdulazizs-MacBook-Air.local>
2019-12-17 16:29:42 +0300

    edit password in app_test.py

--- app_test.py
+++ app_test.py
@@ -8,23 +8,33 @@ class TestUser(unittest.TestCase):
     def setUp(self):
         '''set up a method to run before each case '''
 
-        self.new_user = User("Suad" , "deepinthot")
+        self.new_user = User("Suad" , "samar99")
         self.generate = Credentials ("twitter", "lordSpiral", "sitUp!")
 
     def test_init(self):
             '''Testing to see if the object is properly initialized'''
 
         self.assertEqual(self.new_user.username, "Suad")
-        self.assertEqual(self.new_user.password, "deepinthot")
+        self.assertEqual(self.new_user.password, "samar99")
 
         self.assertEqual(self.generate.app_title, "twitter")
         self.assertEqual(self.generate.acc_name, "lordSpiral")
         self.assertEqual(self.generate.acc_password, "sitUp!")
 
-        def test_saved_acc(self):
+    def test_saved_acc(self):
             '''Test case saving username and passwords'''
 
         self.assertEqual(len(User.account_list), 0)
+
+    def test_user_auth(self):
+            '''Test is user login'''
+
+        self.new_user.save_acc()
+        test_user = User ("suad", "samar99")
+        test_user.save_acc()
+
+        user_exist = User.user_auth("suad", "samar99")
+
 
 
 
--- credentials.py
+++ credentials.py
@@ -20,7 +20,7 @@ class Credentials:
 
              Credentials.generated.append(self)
 
-             def delete_data(self):
+    def delete_data(self):
             '''Method to delete an account of our list'''
 
         Credentials.generated.remove(self)
