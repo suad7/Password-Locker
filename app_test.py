1cd7ce595ed555fd2387a7691be73ed45e46b518
Suad shire <jido@abdulazizs-MacBook-Air.local>
2019-12-17 14:54:30 +0300

    ipmorted  Credentials in app_test.py

--- app_test.py
+++ app_test.py
@@ -1,3 +1,10 @@
 import unittest 
 from user import User
 from credentials import Credentials
+
+class TestUser(unittest.TestCase):
+    '''defines test cases for user methods'''
+
+    def setUp(self):
+        '''set up a method to run before each case '''
+
---
+++ credentials.py
@@ -0,0 +1,16 @@
+import random
+import pyperclip
+import string
+
+
+class Credentials:
+    '''class to instanciate new account credentials'''
+
+    generated = []
+
+def __init__(self, app_title, acc_name, acc_password):
+        '''Method to instanciate object properties'''
+
+        self.app_title = app_title
+        self.acc_name = acc_name
+        self.acc_password = acc_password
