149241f19be7abb52be09bf0d696b115cb50e2e4
Suad shire <jido@abdulazizs-MacBook-Air.local>
2019-12-17 15:56:41 +0300

    added function test init in app_test

--- app_test.py
+++ app_test.py
@@ -7,4 +7,11 @@ class TestUser(unittest.TestCase):
 
     def setUp(self):
         '''set up a method to run before each case '''
+        self.new_user = User("Suad7")
+        self.generate = Credentials ("twitter", "lordSpiral", "sitUp!")
+        def test_init(self):
+            '''Testing to see if the object is properly initialized'''
+
+
+
 
